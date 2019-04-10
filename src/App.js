33bdb652b72271319c1d896c71ea1d58f22e4eb4
Pete Ptgomez Tiffany <ptgomez@Methuselah.local>
2019-04-10 16:34:37 -0700

    Updated with main photo for initial site

--- src/App.js
+++ src/App.js
@@ -1,14 +1,13 @@
 import React, { Component } from 'react';
 import './App.css';
+import main from './images/v0_main.jpg';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          <p>
-            Welcome to ByTheBook: The Bible!
-          </p>
+          <img src={main} alt={"Main Site"} />
         </header>
       </div>
     );
