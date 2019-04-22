3079d69094cbd192c3fedba60891f0178ff0bff9
Pete Gomez <pete.gomez@concur.com>
2019-04-22 10:45:33 -0700

    Updates, favicon

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,11 +1,17 @@
 import React, { Component } from 'react';
 import '../css/Main.css';
+import logo_animated from "../images/frontanimation_600px.gif";
+import under_construction from "../images/webconstructionsite.png";
 
 export default class Main extends Component {
     render() {
         return (
             <div className="Main-body">
-                Test!
+                <br />
+                <img src={logo_animated} alt={"Animated Front Logo"} />
+                <br />
+                <br />
+                <img src={under_construction} alt={"Site Presently Under Construction"} />
             </div>
         );
     }
