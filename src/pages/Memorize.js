a071718c0b6bf61c830066891b99e1ec1308ca3c
blackeaglebot <blackeaglebot@live.com>
2020-06-24 00:19:03 -0700

    prettier controls

--- src/pages/Memorize.js
+++ src/pages/Memorize.js
@@ -7,14 +7,24 @@ import {
   BigPlayButton
 } from 'video-react';
 import "../../node_modules/video-react/dist/video-react.css"
-import {Row, Col, ToggleButton, ToggleButtonGroup, ButtonGroup, Dropdown} from 'react-bootstrap'
+import {Row, Col, ToggleButton, ToggleButtonGroup, ButtonGroup, Dropdown, Container} from 'react-bootstrap'
 import $ from "jquery"
 import logo from '../images/logo.svg';
 
 import {Login} from '../forms/Login.js'
 
 import videoSplash from "../images/videoSplash.png"
-import { useAuth } from '../hooks';
+import { useAuth, useFirestore, useCachedStorage } from '../hooks';
+
+import WhatItMeans  from '../images/memoryKinds/WhatItMeans.svg'
+import Watch        from '../images/memoryKinds/Watch.svg'
+import SpeedMemory  from '../images/memoryKinds/SpeedMemory.svg'
+import Schmoment    from '../images/memoryKinds/Schmoment.svg'
+import Music        from '../images/memoryKinds/Music.svg'
+import Karaoke      from '../images/memoryKinds/Karaoke.svg'
+import FamilyChat   from '../images/memoryKinds/FamilyChat.svg'
+import Dance        from '../images/memoryKinds/Dance.svg'
+import Color        from '../images/memoryKinds/Color.svg'
 
 var firebase = require('firebase')
 var db = firebase.firestore()
@@ -30,7 +40,29 @@ const books = ['Genesis', 'Exodus', 'Leviticus', 'Numbers', 'Deuteronomy',
 '1 Timothy', '2 Timothy', 'Titus', 'Philemon', 'Hebrews', 'James',
 '1 Peter', '2 Peter', '1 John', '2 John', '3 John', 'Jude', 'Revelation']
 
-const kinds = ["Music Video", "Dance Video", "Karaoke Video", "Coloring Pages", "Teachers Guide"]
+// const kinds = ["Music Video", "Dance Video", "Karaoke Video", "Coloring Pages", "Teachers Guide"]
+// const kinds = {
+//     "Watch": Watch,
+//     "What It Means": WhatItMeans,
+//     "Speed Memory": SpeedMemory,
+//     "Schmoment": Schmoment,
+//     "Music": Music,
+//     "Karaoke": Karaoke,
+//     "Family Chat": FamilyChat,
+//     "Dance": Dance,
+//     "Color": Color,
+// }
+const kinds = {
+    "Music Video": Watch,
+    // "What It Means": WhatItMeans,
+    // "Speed Memory": SpeedMemory,
+    // "Schmoment": Schmoment,
+    // "Music": Music,
+    "Dance Video": Dance,
+    "Teachers Guide": FamilyChat,
+    "Coloring Pages": Color,
+    // "Karaoke Video": Karaoke,
+}
 
 // convert between scripture references and a string key
 // used for tracking scripture selected
@@ -43,13 +75,13 @@ const scriptureFromKey = key => {
 // this is a mathematically correct mod accounting for negative numbers
 // mod(n, m) returns i where 0 <= i < m, where n - i is divisible by m
 function mod(n, m) {
-  return ((n % m) + m) % m;
+  return m >= 0 ? n % m : (n % m) + m
 }
 
 // Media players for each kind of resource
 function PDFMedia(props) {
     return [
-        <div style={{maxWidth:'1000px', margin:'auto'}} className="embed-responsive embed-responsive-17by22" >
+        <div className="player embed-responsive embed-responsive-17by22" >
             <object data={props.src} type='application/pdf' style={{overflow:'scroll'}}></object>
         </div>,
         <br/>,<a href={props.src}>Download</a>
@@ -57,15 +89,13 @@ function PDFMedia(props) {
 }
 
 function VideoMedia(props) {
-    return <div style={{maxWidth:'1000px', margin:'auto'}} >
-        <Player playsInline src={props.src} className="player" poster={videoSplash}>
-            <BigPlayButton position="center" />
-            <ControlBar>
-                <PlaybackRateMenuButton rates={[2.0,1.5,1.0,0.7,0.5]} order={7.1} />
-                <VolumeMenuButton order={7.1} vertical/>
-            </ControlBar>
-        </Player>
-    </div>
+    return <Player playsInline src={props.src} className="player" poster={videoSplash}>
+        <BigPlayButton position="center" />
+        <ControlBar>
+            <PlaybackRateMenuButton rates={[2.0,1.5,1.0,0.7,0.5]} order={7.1} />
+            <VolumeMenuButton order={7.1} vertical/>
+        </ControlBar>
+    </Player>
 }
 
 let players = {
@@ -73,105 +103,67 @@ let players = {
     "pdf": PDFMedia,
 }
 
-export default function Memorize() {
+let defaultSelected = new Set(["39-007-00001-6", "39-007-0007-11", "39-007-0012-14", "39-007-0015-20", "39-007-0021-23", "39-007-0024-29"])
+
+export default function Memorize(props) {
     // Initialize and load resources and selections
-    let [resources, setResources] = useState({})
-    let [scriptureSelected, setScriptureSelected] = useState(new Set())
-    let [kindsSelected, setKindsSelected] = useState(new Set([kinds[0]]))
+    let [scriptureSelected, setScriptureSelected] = useState(defaultSelected)
+    // let [kindsSelected, setKindsSelected] = useState(new Set([kinds[0]]))
+    let [kindsSelected, setKindsSelected] = useState(new Set([Object.keys(kinds)[0]]))
 
-    // load list of video resources
-    useEffect(() => {
-        let mounted = true
-        async function getResources() {
-            let snapshot = await db.collection('memoryResources').get()
-            let scriptureSelected = new Set()
-            let resources = snapshot.docs.reduce((r, doc) => {
-                let d = doc.data()
-                let verses = `${d.startVerse}-${d.endVerse}`
-                let key = keyFromScripture(d.book,d.chapter,verses)
-                r[key] = r[key] || {}
-                r[key][d.kind] = d
-                scriptureSelected.add(key)
-                return r
-            }, {})
-
-            if(mounted) {
-                setResources(resources) // Must set resources before setting scriptureSelected
-                setScriptureSelected(scriptureSelected)
-            }
-        }
-        getResources()
-        return () => mounted = false;
-    }, [])
-
+    let resources = useFirestore('memoryResources', (cum, doc) => {
+        let d = doc.data()
+        let verses = `${d.startVerse}-${d.endVerse}`
+        let key = keyFromScripture(d.book,d.chapter,verses)
+        cum[key] = cum[key] || {}
+        cum[key][d.kind] = d
+        return cum
+    }, {})
+    
     // Adjust view for access level
     let [user, claims] = useAuth(true)
-
+    
     let upgradeMsg
-    if (!user) {
-        // Should log in
+    if (!user) {// Should log in
         upgradeMsg = <div><Login.LoginButton/> for free trial</div>
     } else if(claims.expirationDate - Date.now() > 0 || claims.permanentAccess || claims.admin) {
-        // Full access
-        upgradeMsg = null
-    } else {
-        // Should subscribe
+        upgradeMsg = null // Full access
+    } else { // Should subscribe
         upgradeMsg = <div><a href='/subscribe'>Subscribe to see everything</a></div>
     }
-
+    
     // Choose correct resource to play
-    let [urlList, setUrlList] = useState([])
     let [index, setIndex] = useState(0)
-    let [url, setUrl] = useState(null)
-
+    let [selectedResources, setSelectedResources] = useState([])
+    let [urlList, setUrlList] = useState([])
+    
     // update list of urls matching selection
     useEffect(() => {
-        let newUrlList = []
-        for(let key of scriptureSelected) {
-            for(let kind of kindsSelected) {
-                if(kind in resources[key]) {
-                    newUrlList.push(resources[key][kind].url)
+        if(resources != undefined) {
+            let newUrlList = []
+            for(let key of scriptureSelected) {
+                for(let kind of kindsSelected) {
+                    if(kind in resources[key]) {
+                        newUrlList.push(resources[key][kind].url)
+                    }
                 }
             }
+            setUrlList(newUrlList)
+            setIndex(0)
         }
-        setUrlList(newUrlList)
     }, [resources, scriptureSelected, kindsSelected])
 
     // get video from cache if available
     let keyUrl = urlList[mod(index, urlList.length)]
-    useEffect(() => {
-        var cancel = false
-        async function fetchUrls() {
-            let newUrl = await getVideoUrl(keyUrl)
-            // console.log('newUrl', newUrl)
-            if(!cancel) {
-                URL.revokeObjectURL(url)
-                url = undefined
-                setUrl(newUrl)
-            } else {
-                URL.revokeObjectURL(newUrl)
-                newUrl = undefined
-            }
-            // premptively fetch prev / next videos if needed
-            preloadVideos([
-                urlList[mod(index + 1, urlList.length)], 
-                // urlList[mod(index - 1, urlList.length)]
-            ])
-        }
-        fetchUrls()
-
-        return () => cancel = true
-    }, [urlList, index])
-
-
+    let url = useCachedStorage({url: keyUrl, version: '-1'})
 
     // Choose correct player for current url
     let fileKind = keyUrl && keyUrl.split('.').slice(-1)[0]
     let player = keyUrl && players[fileKind]
 
     // Memory format selector
-    let memoryFormatSelector = <div className='text-center'>
-        {kinds.map((kind) => 
+    let memoryFormatSelector = <div className='text-center' style={{bottom:0, zIndex:1}}>
+        {Object.keys(kinds).map((kind) => 
             <ToggleButtonGroup className='checkbox' type='checkbox' key={kind} defaultValue={kindsSelected.has(kind) ? kind : []} onChange={values => {
                 let k = new Set(kindsSelected)
                 if(!!values[0]) {k.add(kind)}
@@ -179,150 +171,82 @@ export default function Memorize() {
                 setKindsSelected(k)
                 setIndex(0)
             }}>
-                <ToggleButton value={kind} variant='outline-primary'>{kind}</ToggleButton>
+                <ToggleButton value={kind} variant='outline-primary'><div style={{WebkitMask:`url(${kinds[kind]})`, mask:`url(${kinds[kind]})`, maskSize: "cover"}}></div></ToggleButton>
             </ToggleButtonGroup>
         )}
     </div>
 
+    let [showControls, setShowControls] = useState(true)
+
     // Page layout
-    return (<div className="container-xl py-2" >
-        {/* {scriptureSelector} */}
-        <ScriptureSelector defaultValue={scriptureSelected} resources={resources} onChange={v => {setScriptureSelected(v)}} />
-        <Row>
-            <i className="fa fa-5x fa-chevron-left player-control-prev" onClick={() => setIndex(index - 1) } />
-            <Col>
-                {player && player({src: url})}
-            </Col>
-            <i className="fa fa-5x fa-chevron-right player-control-next" onClick={() => setIndex(index + 1) }/>
-        </Row>
+    return <div className={`container-xl py-2 memorize-controls ${showControls ? '' : 'hide'}`} onMouseMove={mouseMoving(setShowControls)}>
+        {player && player({src: url, style: {position:'absolute', zIndex:1}})}
+        <ScriptureSelector key='scripture-selector' className='scripture-selector' selected={scriptureSelected} resources={resources} onChange={v => {setScriptureSelected(v)}} />
+        <i className="fa fa-4x fa-chevron-left player-control-prev" onClick={() => setIndex(index - 1) } />
+        <i className="fa fa-4x fa-chevron-right player-control-next" onClick={() => setIndex(index + 1) }/>
         {memoryFormatSelector}
-        {upgradeMsg}
-    </div>)
+        {/* {upgradeMsg} */}
+    </div>
+}
+
+let timeout
+let mouseMoving = (cb) => () => {
+    cb(true);
+    
+    (() => {
+        clearTimeout(timeout);
+        timeout = setTimeout(() => cb(false), 5000);
+    })();
 }
 
 function ScriptureSelector(props) {
     let [show, setShow] = useState(!!props.defaultOpen) // bool: true if expanded
-    let [value, setValue] = useState(new Set()) // list of scripture key strings which are currently selected
-    let [scriptures, setScriptures] = useState({})
+    let [selected, setSelected] = useState(new Set(props.selected)) // list of scripture key strings which are currently selected
 
-    useEffect(() => {
-        setValue(new Set(props.defaultValue))
-    }, [props.defaultValue])
+    if(!props.resources) return null
 
-    useEffect(() => {
-        // Make scripture grouped by Book, Chapter
-        let scriptures = Object.keys(props.resources).reduce((acc, key) => {
-            let s = scriptureFromKey(key)
-            acc[s.book] = acc[s.book] || {}
-            acc[s.book][s.chapter] = acc[s.book][s.chapter] || {}
-            acc[s.book][s.chapter][s.verses] = props.resources[key]
-            
-            // let bookChapter = `${s.book} ${s.chapter}`
-            // acc[bookChapter] = acc[bookChapter] ? [...acc[bookChapter], key] : [key]
-            return acc
-        }, {})
-        setScriptures(scriptures)
+    // Make scripture grouped by Book, Chapter
+    let scriptures = Object.keys(props.resources).reduce((cum, key) => {
+        let s = scriptureFromKey(key)
+        cum[s.book] = cum[s.book] || {}
+        cum[s.book][s.chapter] = cum[s.book][s.chapter] || {}
+        cum[s.book][s.chapter][s.verses] = s
+        return cum
+    }, {})
 
-    }, [props.resources])
+    let title = "Choose Scripture..."
+    if(!show && selected.size > 0) {
+        let s = scriptureFromKey(selected.keys().next().value)
+        title = `${s.book} ${s.chapter}`
+    }
 
-    let title = "What do you want to memorize?"
-    if(!show) {
-        if(value.size > 0) {
-            let s = scriptureFromKey(value.keys().next().value)
-            title = `${s.book} ${s.chapter}`
-        }
-
-        return <div className='selection-expand-box text-center' onMouseEnter={() => setShow(true)} onClick={() => setShow(true)}>
+    return <div className={`selection-expand-box ${show ? 'show' : ''} ${props.className || ''}`}
+                onMouseLeave={() => setShow(false)} onMouseEnter={() => setShow(true)} onClick={() => setShow(true)}>
+        <div className='text-center' onClick={() => setShow(false)}>
             <img src={logo} height="30rem"/>
             <h2>{title}</h2>
         </div>
-    } else {
-        return <div className='selection-expand-box show' onMouseLeave={() => setShow(false)}>
-            <div className='text-center' onClick={() => setShow(false)}>
-                <img src={logo} height="30rem"/>
-                <h2>{title}</h2>
-            </div>
 
-            {show &&
-            <Row>{Object.keys(scriptures).map(book => <Col>
-                <h3>{book}</h3>
-                <Row>{Object.keys(scriptures[book]).map(chapter => <Col>
-                    <h3>{chapter}</h3>
-                    <div className='wavy-col' >
-                        <div style={{height:'2.5rem', gridArea:'1 / 2 / 1 / 2'}} ></div>
-                        {Object.keys(scriptures[book][chapter]).map(verses => {
-                            let key = keyFromScripture(book, chapter, verses)
-                            return <ToggleButtonGroup className='checkbox' type='checkbox' key={key} defaultValue={value.has(key) ? key : []} onChange={values => {
-                                var v = new Set(value)
-                                if(!!values[0]) v.add(key)
-                                else v.delete(key)
-                                setValue(v)
-                                props.onChange(v)
-                            }}>
-                                <ToggleButton value={key} variant='outline-primary'>{verses}</ToggleButton>
-                            </ToggleButtonGroup>}
-                        )}
-                    </div>
-                </Col>)}</Row>
-            </Col>)}</Row>}
-        </div>
-    }
-}
-
-// preloadVideos(['memory/Matthew/007/39-007-001-006-music-video.mp4'])
-
-const cacheName = 'btbtb'
-const maxCache = 8 // safari warned at 1.2G
-
-async function getVideoUrl(url) {
-    if(!url)
-        return url
-    if(caches) {
-        let cache = await caches.open(cacheName)
-        let res = await cache.match(url)
-        if(res) {
-            let blob = await res.blob()
-            return URL.createObjectURL(blob)
-        } else {
-            preloadVideos([url])
-        }
-    }
-
-    const downloadUrl = await storage.ref(url).getDownloadURL()
-    return downloadUrl
-}
-
-async function preloadVideos(urls) {
-    if(!caches) return
-    let cache = await caches.open(cacheName)
-
-    let keys = await cache.keys()
-    for(let i in urls) {
-        let url = urls[i]
-        if(!url) continue
-        let res = await cache.match(url)
-        if(res) {
-            res = undefined
-            continue
-        }
-
-        let downloadUrl = await storage.ref(url).getDownloadURL()
-        console.log(downloadUrl)
-
-        // cannot get without setting configuring CORS from gsutil with
-        // gsutil cors set cors.json gs://bythebookthebible.appspot.com
-        res = await fetch(downloadUrl, {
-            method:'GET',
-            mode:'cors',
-            // credentials:'include',
-        })
-        console.log(url, downloadUrl, res)
-        await cache.put(url, res)
-        res = undefined
-    }
-    
-    if(keys.length > maxCache) {
-        // if too many items stored, remove some
-        
-    }
+        {show &&
+        <Container><Row style={{flexWrap:'nowrap', justifyContent:'flex-start'}}>{Object.keys(scriptures).map(book => <>
+            {Object.keys(scriptures[book]).map(chapter => <Col>
+                <h3>{book + ' ' + chapter}</h3>
+                <div className='wavy-col'>
+                    <div style={{height:'2rem', gridArea:'1 / 2 / 1 / 2'}} ></div>
+                    {Object.keys(scriptures[book][chapter]).map(verses => {
+                        let key = keyFromScripture(book, chapter, verses)
+                        return <ToggleButtonGroup className='checkbox' type='checkbox' key={key} defaultValue={selected.has(key) ? key : []} onChange={newSelected => {
+                            var v = new Set(selected)
+                            if(!!newSelected[0]) v.add(key)
+                            else v.delete(key)
+                            props.onChange(v)
+                            setSelected(v)
+                        }}>
+                            <ToggleButton selected={key} value={key} variant='outline-primary'>{verses}</ToggleButton>
+                        </ToggleButtonGroup>}
+                    )}
+                </div>
+            </Col>)}
+        </>)}</Row></Container>}
+    </div>
 }
