c74e9daa8bede44c73f4d88e6553d1f70dedee36
blackeaglebot <blackeaglebot@live.com>
2020-07-20 17:38:10 -0700

    palace doors map

--- src/forKids/kidModeApp.js
+++ src/forKids/kidModeApp.js
@@ -1,14 +1,12 @@
 import React, { Component, useState, useEffect, useRef, useReducer} from 'react'
-
-import mainMap from '../images/maps/MainMap.svg'
-import readingTree from '../images/maps/TestTree.svg'
-import ButtonMap from './buttonMap'
 import Activity from './activity'
 
 import { media } from "./media";
 import { useAuth, useFirestore, useCachedStorage } from "../hooks"
 import { keyFromScripture, scriptureFromKey, valueAfter, pathFilters } from "../util"
 import { Spinner } from 'react-bootstrap'
+
+import {Maps} from './maps'
 
 export var DispatchContext = React.createContext(undefined)
 export var StateContext = React.createContext(undefined)
@@ -107,36 +105,36 @@ function getPathActivities(resources, path) {
         }, [])
 }
 
-let Tree = props => <div>
-    <h1>Tree</h1>
-    <ButtonMap src={readingTree} buttons={[
-        {id:'Palace', dispatch: {type:'newView', view:'map', map:'home'}},
-        {id:'Branch1', dispatch: {type:'newView', view:'activity', activity:{key:'39-007-0001-10', kind:'Music Video'}}},
-        {id:'Branch2', dispatch: {type:'newView', view:'activity', activity:{key:'39-007-0007-11', kind:'Music Video'}}},
-        {id:'Branch3', dispatch: {type:'newView', view:'activity', activity:{key:'39-007-0012-14', kind:'Music Video'}}},
-        {id:'Branch4', dispatch: {type:'newView', view:'activity', activity:{key:'39-007-0015-20', kind:'Music Video'}}},
-        {id:'Branch5', dispatch: {type:'newView', view:'activity', activity:{key:'39-007-0021-23', kind:'Music Video'}}},
-        {id:'Branch6', dispatch: {type:'newView', view:'activity', activity:{key:'39-007-0024-29', kind:'Music Video'}}},
-    ]}/>
-</div>
+// let Tree = props => <div>
+//     <h1>Tree</h1>
+//     <ButtonMap src={readingTree} buttons={[
+//         {id:'Palace', dispatch: {type:'newView', view:'map', map:'home'}},
+//         {id:'Branch1', dispatch: {type:'newView', view:'activity', activity:{key:'39-007-0001-10', kind:'Music Video'}}},
+//         {id:'Branch2', dispatch: {type:'newView', view:'activity', activity:{key:'39-007-0007-11', kind:'Music Video'}}},
+//         {id:'Branch3', dispatch: {type:'newView', view:'activity', activity:{key:'39-007-0012-14', kind:'Music Video'}}},
+//         {id:'Branch4', dispatch: {type:'newView', view:'activity', activity:{key:'39-007-0015-20', kind:'Music Video'}}},
+//         {id:'Branch5', dispatch: {type:'newView', view:'activity', activity:{key:'39-007-0021-23', kind:'Music Video'}}},
+//         {id:'Branch6', dispatch: {type:'newView', view:'activity', activity:{key:'39-007-0024-29', kind:'Music Video'}}},
+//     ]}/>
+// </div>
 
-let Map = props => <div>
-    <ButtonMap src={mainMap} buttons={[
-        {id:'City_Center', dispatch: {type:'newView', view:'map', map:'tree'}},
-        {id:'Water_Well', dispatch: {type:'newView', view:'map', map:'tree'}},
-        {id:'Jo_Schmo_House', dispatch: {type:'newView', view:'map', map:'tree'}},
-        {id:'Art_Gazebo', dispatch: {type:'newView', view:'map', map:'tree'}},
-        {id:'Dragon', dispatch: {type:'newView', view:'map', map:'tree'}},
-        {id:'Memory_Palace', dispatch: {type:'newView', view:'map', map:'tree'}},
-        {id:'Game_Factory', dispatch: {type:'newView', view:'map', map:'tree'}},
-        {id:'Book_Tree', dispatch: {type:'newView', view:'map', map:'tree'}},
-    ]}/>
-</div>
+// let Map = props => <div>
+//     <ButtonMap src={mainMap} buttons={[
+//         {id:'City_Center', dispatch: {type:'newView', view:'map', map:'tree'}},
+//         {id:'Water_Well', dispatch: {type:'newView', view:'map', map:'tree'}},
+//         {id:'Jo_Schmo_House', dispatch: {type:'newView', view:'map', map:'tree'}},
+//         {id:'Art_Gazebo', dispatch: {type:'newView', view:'map', map:'tree'}},
+//         {id:'Dragon', dispatch: {type:'newView', view:'map', map:'tree'}},
+//         {id:'Memory_Palace', dispatch: {type:'newView', view:'map', map:'tree'}},
+//         {id:'Game_Factory', dispatch: {type:'newView', view:'map', map:'tree'}},
+//         {id:'Book_Tree', dispatch: {type:'newView', view:'map', map:'tree'}},
+//     ]}/>
+// </div>
 
-let Maps = {
-    home:Map,
-    tree:Tree,
-}
+// let Maps = {
+//     home:Map,
+//     tree:Tree,
+// }
 
 export default function KidModeApp(props) {
     let [state, dispatch] = useReducer(kidAppReducer, {view:'loading'})
@@ -185,7 +183,7 @@ export default function KidModeApp(props) {
     // @TODO: add component when the powerLevel is over 100, prompt the user to send in a video/ zoom Rose and Catherine
     let content = <div className='text-center pt-3'><Spinner animation="border" role="status" size="md" /><h1 className='d-inline-block'>Loading...</h1></div>
     if(state.view == 'map') content = Maps[state.map]()
-    if(state.view == 'tree') content = <Tree />
+    // if(state.view == 'tree') content = <Tree />
     if(state.view == 'activity') content = <Activity actKey={state.activity.key} actKind={state.activity.kind} resources={resources} />
 
     return <DispatchContext.Provider value={dispatch}><StateContext.Provider value={state}>
