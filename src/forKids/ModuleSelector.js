0b851141d24c538fffd0c0ebd7de491dcbfab0a8
tiphanieleung <leungti@uw.edu>
2020-07-23 01:41:40 -0700

    add dragon view

--- src/forKids/ModuleSelector.js
+++ src/forKids/ModuleSelector.js
@@ -25,12 +25,8 @@ function ModuleSelctor(props) {
     return cum
   }, {})
 
-  let resizeBackground = (chapterBackground) => {
-    let background = 'url(' + chapterBackground + ")"
-    
-  }
-
-  return <div style={{minHeight:'90vh', position:'relative', backgroundImage: "url(" + props.background + ")", ...props.style}}>
+  if (props.module == 'book') {
+    return <div style={{minHeight:'90vh', position:'relative', backgroundImage: "url(" + props.background + ")", ...props.style}}>
     <Container fluid style={{...props.style, paddingLeft: '0', paddingRight: '0'}}>
     {/* <div style={{backgroundOrigin: 'content-box', backgroundImage: "url(" + props.chapterBackground + ")", backgroundRepeat: 'none', backgroundAttachment: 'local'}}> */}
       {Object.keys(scriptures).map(book =>
@@ -40,22 +36,46 @@ function ModuleSelctor(props) {
           </Row>
           <Row style={{ marginLeft: '30px', marginRight: '30px', justifyContent: 'flex-start', flexWrap: 'nowrap', overflowX: 'auto' }}>
             {Object.keys(scriptures[book][chapter]).map(verses =>
+              //content
               <Col sm={2} style={{ }}><img src={tree} style={{width: '60px', height: '60px'}} /><br></br>{verses}</Col>
             )}
           </Row>
-          {/* <Row> 
-            {Object.keys(scriptures[book][chapter]).map(verses =>
-              <Col>{verses}</Col>
-            )}
-          </Row> */}
         </>)
       )}
-      {/* </div> */}
     </Container>
   </div>
+  } else if (props.module == 'dragon') {
+    return <div>
+    <Container fluid style={{...props.style, paddingLeft: '0', paddingRight: '0'}}>
+    <div style={{marginTop: '-55px', marginBottom: '-70px',backgroundSize: 'cover, contain', backgroundImage: "url(" + props.background + ")", backgroundRepeat: 'no-repeat', backgroundAttachment: 'local'}}>
+      <div style={{padding: '70px', paddingBottom: '100px'}}>
+      {Object.keys(scriptures).map(book =>
+        Object.keys(scriptures[book]).map(chapter => <>
+          <Row style={{position:'relative', backgroundImage: "url(" + props.chapterBackground + ")", backgroundRepeat: 'no-repeat', right: '-50%'}}>
+            <Col style={{ padding: '5%', fontSize: '1.4rem', fontWeight: 'bold', color: 'black' }}>{`${book} ${chapter}`}</Col>
+          </Row>
+          <Row style={{ marginLeft:'50%', justifyContent: 'flex-start', flexWrap: 'nowrap', overflowX: 'auto' }}>
+            {Object.keys(scriptures[book][chapter]).map(verses =>
+              //content
+              <Col sm={4} style={{ }}>
+                <div>
+                  <img src={tree} style={{width: '60px', height: '60px'}} /><br></br>{verses}
+                </div>
+              </Col>
+            )}
+
+          </Row>
+        </>)
+      )}
+      </div>
+      </div>
+    </Container>
+  </div>
+  }
+
 }
 
 export default {
-  dragon: <ModuleSelctor background={karaokeBg} chapterBackground={karaokeBar} />,
-  book: <ModuleSelctor style={{backgroundColor:"#dd08", minHeight: '100vh'}} chapterBackground={bookBar} />,
+  dragon: <ModuleSelctor background={karaokeBg} chapterBackground={karaokeBar} module='dragon' />,
+  book: <ModuleSelctor style={{backgroundColor:"#dd08", minHeight: '100vh'}} chapterBackground={bookBar} module='book' />,
 }
