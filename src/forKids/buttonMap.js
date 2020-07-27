4e5447a724881b8ac9f21f092f05fcea175fbfe2
blackeaglebot <blackeaglebot@live.com>
2020-07-27 16:12:26 -0700

    glowing hover on maps

--- src/forKids/buttonMap.js
+++ src/forKids/buttonMap.js
@@ -7,16 +7,25 @@ import { DispatchContext } from "./kidModeApp"
 export default function ButtonMap(props) {
     let dispatch = useContext(DispatchContext)
 
-    return <ReactSVG src={props.src} afterInjection={(err, svg) => {
+    return <ReactSVG src={props.src} renumerateIRIElements={false}
+        afterInjection={(err, svg) => {
         for (let button of props.buttons) {
-            // $("#" + button.id).hover(function() {
-                // $("#" + button.id).attr({ filter: 'url(#shadowHover)'})
-                // setAttribute({ filter: 'url(#shadowHover)'})
-            // });
             $("#" + button.id).click(() => {
                 button.dispatch && dispatch(button.dispatch)
                 button.onClick && button.onClick()
-            });
+            })
+            .hover(
+                // ReactSVG seems to add "-1" to the end of the id. Hence #glow-1
+                e=>{$("#" + button.id).attr({ filter: 'url(#glow)'})},
+                e=>{$("#" + button.id).attr({ filter: ''})},
+            )
         }
     }}/>
 }
+
+// let filter = $('\
+// <filter id="glow">\
+//     <feDropShadow dx="0" dy="0" stdDeviation="10" floodColor="white" result="shadow"></feDropShadow>\
+//     <feBlend in="SourceGraphic" in2="shadow" mode="normal"></feBlend>\
+// </filter>\
+// ')
