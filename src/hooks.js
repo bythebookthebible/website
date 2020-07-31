e76d2b2c4689d63a032ebe4d47977f120a11d132
tiphanieleung <leungti@uw.edu>
2020-07-30 19:40:02 -0700

    fix merging error

--- src/hooks.js
+++ src/hooks.js
@@ -1,4 +1,4 @@
-import React, {useState, useEffect, useRef} from 'react'
+import React, {useState, useEffect} from 'react'
 import {openDB, deleteDB, wrap, unwrap} from 'idb'
 import deepEqual from 'deep-equal'
 import { diff } from 'deep-object-diff'
@@ -22,15 +22,6 @@ import {firebase, db, storage} from './firebase'
 //     return useClaims ? [user, claims] : user
 // }
 
-// this pattern is used enough I wanted to encapsulate it
-export function useAsyncEffect(fn=()=>undefined, deps=[]) {
-    let abort = useRef(false)
-    useEffect(() => {
-        fn(abort)
-        return ()=>abort.current=true
-    }, deps)
-}
-
 export var withAuth = wrappedComponent => props => {
     let [user, setUser] = useState(firebase.auth().currentUser)
     let [claims, setClaims] = useState(undefined)
@@ -50,9 +41,14 @@ export var withAuth = wrappedComponent => props => {
         setClaims(newClaims)
     }
 
-    useAsyncEffect(async abort => {
-        let newClaims = user && (await user.getIdTokenResult()).claims
-        setClaims(newClaims)
+    useEffect(()=>{
+        let abort=false
+        async function updateClaims() {
+            let newClaims = user && (await user.getIdTokenResult()).claims
+            if(!abort) setClaims(newClaims)
+        }
+        updateClaims()
+        return ()=>abort=true
     }, [user])
 
     return wrappedComponent({...props, 'user': user && claims && {...user, 'claims': claims}, refreshUser:refresh})
@@ -65,13 +61,20 @@ export function useFirestore(collection, reduceFn=undefined, reduceInit={}) {
     }
 
     // load list of video resources
-    useAsyncEffect(async abort => {
-        let snapshot = await db.collection(collection).get()
-        let resources = snapshot.docs.reduce(reduceFn, reduceInit)
+    useEffect(() => {
+        let abort = false
+        async function getResources() {
+            let snapshot = await db.collection(collection).get()
+            let resources = snapshot.docs.reduce(reduceFn, reduceInit)
 
-        if(!abort.current) setResources(resources)
+            if(!abort) {
+                setResources(resources) // Must set resources before setting scriptureSelected
+            }
+        }
+        getResources()
+        return () => abort = true;
     }, [])
-    
+
     return resources
 }
 
@@ -79,20 +82,25 @@ export function useFirestoreState(ref, onload) {
     let [data, setData] = useState(undefined)
 
     // initial fetch from firestore
-    useAsyncEffect(async abort => {
-        let snapshot = await db.doc(ref).get()
-        let data = snapshot.data() || {} // create new object if not exist
-        console.log('data from firebase', data)
-        // .docs.reduce(
-        //     (cum, doc) => { cum[doc.id]=doc.data(); return cum }, {}
-        // )
+    useEffect(()=>{
+        let abort = false
+        async function getResources() {
+            let snapshot = await db.doc(ref).get()
+            let data = snapshot.data() || {} // create new object if not exist
+            console.log('data from firebase', data)
+            // .docs.reduce(
+            //     (cum, doc) => { cum[doc.id]=doc.data(); return cum }, {}
+            // )
 
-        if(!abort.current) {
-            setData(data)
-            onload(data)
+            if(!abort) {
+                setData(data)
+                onload(data)
+            }
         }
+        getResources()
+        return () => abort = true;
     }, [])
-    
+
     // update function
     function updateData(newData) {
         let d = diff(data, newData)
@@ -123,10 +131,16 @@ export function useIdbState(ref, onload) {
     let [data, setData] = useState(undefined)
 
     // initial fetch from idb
-    useAsyncEffect(async abort => {
-        onload(data)
+    useEffect(()=>{
+        let abort = false
+        async function getResources() {
+            // fetch from idb
+            onload(data)
+        }
+        getResources()
+        return () => abort = true;
     }, [])
-    
+
     // update function
     function updateData(newData) {
         console.log(diff(data, newData))
@@ -189,11 +203,6 @@ export function useCachedStorage(resource) {
                     if(!abort) setUrl(downloadUrl)
                 }
 
-            // serve from cache if available
-            if(res) {
-                console.log('using blob')
-                let blob = await res.blob()
-                if(!abort.current) setUrl(URL.createObjectURL(blob))
             } else {
                 if(resource.url) {
                     const downloadUrl = await storage.ref(resource.url).getDownloadURL()
@@ -204,8 +213,10 @@ export function useCachedStorage(resource) {
                 }
             }
         }
+        getVideoUrl(resource)
+        return () => abort = true;
     }, [resource.url, resource.version])
-    
+
     return url
 }
 
