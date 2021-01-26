86c94bde307867968a5d52ea749afc285deef8ad
blackeaglebot <blackeaglebot@live.com>
2021-01-25 21:10:24 -0500

    add thumbnail option

--- src/playfulMode/moduleSelector/ModuleSelector.js
+++ src/playfulMode/moduleSelector/ModuleSelector.js
@@ -12,6 +12,7 @@ import { useAsyncEffect, useMemoryResources } from "../../common/hooks"
 import { useDispatch, useSelector } from "react-redux"
 import { newView, playfulViews } from "../playfulReducer"
 import { AbsoluteCentered } from "../../common/components"
+import Thumbnail from "./Thumbnail"
 
 export default function ModuleSelctor(props) {
   let dispatch = useDispatch()
@@ -52,7 +53,7 @@ export default function ModuleSelctor(props) {
                   view:playfulViews.activity,
                   viewSelected:{module:scriptures[book][chapter][verses].key, kind: viewSelected}
                 }))}>
-                <Icon module={scriptures[book][chapter][verses].key} />
+                <Thumbnail module={scriptures[book][chapter][verses].key} kind={viewSelected} />
                 {locked && <AbsoluteCentered><img src={lock} /></AbsoluteCentered>}
               </Col>
             })}
@@ -70,50 +71,50 @@ export default function ModuleSelctor(props) {
   </div>
 }
 
-function Icon(props) {
-  let iconRef = useMemoryResources(resources => resources[props.module].icon)
-  let title = useMemoryResources(resources => resources[props.module].title)
-  let verse = scriptureFromKey(props.module)
-  let [src, setSrc] = useState(defaultIcon)
+// function Icon(props) {
+//   let iconRef = useMemoryResources(resources => resources[props.module].icon)
+//   let title = useMemoryResources(resources => resources[props.module].title)
+//   let verse = scriptureFromKey(props.module)
+//   let [src, setSrc] = useState(defaultIcon)
   
-  useAsyncEffect(async abort => {
-    if(iconRef) {
-      const downloadUrl = await storage.ref(iconRef[0]).getDownloadURL()
-      if(!abort.current) setSrc(downloadUrl)
-    }
-  }, [iconRef])
+//   useAsyncEffect(async abort => {
+//     if(iconRef) {
+//       const downloadUrl = await storage.ref(iconRef[0]).getDownloadURL()
+//       if(!abort.current) setSrc(downloadUrl)
+//     }
+//   }, [iconRef])
 
-  return <svg xmlns="http://www.w3.org/2000/svg" xmlnsXlink="http://www.w3.org/1999/xlink" viewBox="0 0 389.01 204.98">
-    <defs>
-      <style>{".cls-1{font - size:44.28px;}.cls-1,.cls-2,.cls-5{fill:#231f20;}.cls-1,.cls-5{font - family:Loopiejuice-Regular, Loopiejuice;}.cls-2{opacity:0.4;}.cls-3{fill:#fff;}.cls-4,.cls-8{fill:none;stroke-miterlimit:10;}.cls-4{stroke:#00bff3;stroke-width:13.17px;}.cls-5{font - size:17.22px;}.cls-6{letter - spacing:-0.02em;}.cls-7{fill:#45ade2;opacity:0.7;}.cls-8{stroke:#fff;stroke-width:5.49px;}"}</style>
-    </defs>
-    <g id="Label">
-      <text class="cls-1" transform="translate(191.1 132.09)">{title}</text>
-    </g>
-    <g id="Icon">
-      <image width="426" height="419" transform="translate(34.38 29.24) scale(0.32)" xlinkHref={src}/>
-  </g>
-    <g id="GreyBackground">
-      <rect class="cls-2" x="5.04" y="6.92" width="374.47" height="191.1" />
-    </g>
-    <g id="WhiteBackground">
-      <g id="Layer_4" data-name="Layer 4">
-        <rect class="cls-3" x="211.82" y="164.14" width="155.52" height="30.86" rx="2.7" />
-      </g>
-    </g>
-    <g id="BlueFrame">
-      <path class="cls-4" d="M382.39,198.44q-80.38-4-166.95-4.58c-72.26-.39-141.52,1.31-207.52,4.58,2.74-28.58,4.5-59.91,4.57-93.63C12.56,69.61,10.76,37,7.92,7.34q88,4,183.19,4.28c66.31.06,130.14-1.48,191.28-4.28C379.57,38,377.84,71.67,378,107.92,378.17,140.31,379.83,170.59,382.39,198.44Z" 
-      transform="translate(-0.64 -0.42)" />
-    </g>
-    <g id="Verse">
-      <text class="cls-5" transform="translate(227.56 181.75)">
-        <tspan class="cls-6">{`${verse.book} ${verse.chapter}:${verse.verses}`}</tspan>
-      </text>
-    </g>
-    <g id="PlayButton">
-      <circle class="cls-7" cx="195.58" cy="102.47" r="56.41" />
-      <circle class="cls-8" cx="195.58" cy="102.47" r="56.41" />
-      <path class="cls-3" d="M187,80l31.31,20.33a3.62,3.62,0,0,1-.06,6.11l-31.31,19.4a3.62,3.62,0,0,1-5.53-3.08V83A3.62,3.62,0,0,1,187,80Z" transform="translate(-0.64 -0.42)" />
-    </g>
-  </svg>
-}
+//   return <svg xmlns="http://www.w3.org/2000/svg" xmlnsXlink="http://www.w3.org/1999/xlink" viewBox="0 0 389.01 204.98">
+//     <defs>
+//       <style>{".cls-1{font - size:44.28px;}.cls-1,.cls-2,.cls-5{fill:#231f20;}.cls-1,.cls-5{font - family:Loopiejuice-Regular, Loopiejuice;}.cls-2{opacity:0.4;}.cls-3{fill:#fff;}.cls-4,.cls-8{fill:none;stroke-miterlimit:10;}.cls-4{stroke:#00bff3;stroke-width:13.17px;}.cls-5{font - size:17.22px;}.cls-6{letter - spacing:-0.02em;}.cls-7{fill:#45ade2;opacity:0.7;}.cls-8{stroke:#fff;stroke-width:5.49px;}"}</style>
+//     </defs>
+//     <g id="Label">
+//       <text class="cls-1" transform="translate(191.1 132.09)">{title}</text>
+//     </g>
+//     <g id="Icon">
+//       <image width="426" height="419" transform="translate(34.38 29.24) scale(0.32)" xlinkHref={src}/>
+//   </g>
+//     <g id="GreyBackground">
+//       <rect class="cls-2" x="5.04" y="6.92" width="374.47" height="191.1" />
+//     </g>
+//     <g id="WhiteBackground">
+//       <g id="Layer_4" data-name="Layer 4">
+//         <rect class="cls-3" x="211.82" y="164.14" width="155.52" height="30.86" rx="2.7" />
+//       </g>
+//     </g>
+//     <g id="BlueFrame">
+//       <path class="cls-4" d="M382.39,198.44q-80.38-4-166.95-4.58c-72.26-.39-141.52,1.31-207.52,4.58,2.74-28.58,4.5-59.91,4.57-93.63C12.56,69.61,10.76,37,7.92,7.34q88,4,183.19,4.28c66.31.06,130.14-1.48,191.28-4.28C379.57,38,377.84,71.67,378,107.92,378.17,140.31,379.83,170.59,382.39,198.44Z" 
+//       transform="translate(-0.64 -0.42)" />
+//     </g>
+//     <g id="Verse">
+//       <text class="cls-5" transform="translate(227.56 181.75)">
+//         <tspan class="cls-6">{`${verse.book} ${verse.chapter}:${verse.verses}`}</tspan>
+//       </text>
+//     </g>
+//     <g id="PlayButton">
+//       <circle class="cls-7" cx="195.58" cy="102.47" r="56.41" />
+//       <circle class="cls-8" cx="195.58" cy="102.47" r="56.41" />
+//       <path class="cls-3" d="M187,80l31.31,20.33a3.62,3.62,0,0,1-.06,6.11l-31.31,19.4a3.62,3.62,0,0,1-5.53-3.08V83A3.62,3.62,0,0,1,187,80Z" transform="translate(-0.64 -0.42)" />
+//     </g>
+//   </svg>
+// }
---
+++ src/playfulMode/moduleSelector/Thumbnail.js
@@ -0,0 +1,95 @@
+import React, { useState } from "react"
+import { scriptureFromKey } from '../../util'
+import defaultIcon from './images/defaultIcon.png'
+import { storage } from "../../firebase"
+import { useAsyncEffect, useMemoryResources } from "../../common/hooks"
+
+export default function Thumbnail({module, kind}) {
+  let iconRef = useMemoryResources(resources => resources[module].icon)
+  let verse = scriptureFromKey(module)
+  let [src, setSrc] = useState(defaultIcon)
+  
+  useAsyncEffect(async abort => {
+    if(iconRef) {
+      const downloadUrl = await storage.ref(iconRef[0]).getDownloadURL()
+      if(!abort.current) setSrc(downloadUrl)
+    }
+  }, [iconRef])
+
+  return (ThumbnailByKind[kind] && ThumbnailByKind[kind]({verse, src})) 
+    || ThumbnailByKind['default']({verse, src})
+}
+
+const ThumbnailByKind = {
+  dragon: ({src, verse}) => null,
+
+  echo: ({src, verse}) => null,
+
+  schmash: ({src, verse}) => null,
+
+  color: ({src, verse}) => null,
+
+  activity: ({src, verse}) => null,
+
+
+  karaoke: ({src, verse}) => null,
+
+  dance: ({src, verse}) => null,
+
+  speedyZoom: ({src, verse}) => null,
+
+  blooper: ({src, verse}) => null,
+
+
+  watch: ({src, verse}) => null,
+
+  discussion: ({src, verse}) => null,
+
+  mySchmo: ({src, verse}) => null,
+
+  colorPrint: ({src, verse}) => null,
+
+  bookmark: ({src, verse}) => null,
+
+  game: ({src, verse}) => null,
+
+  notebook: ({src, verse}) => null,
+
+
+  kidRecite: ({src, verse}) => null,
+
+  intro: ({src, verse}) => null,
+
+  joSchmo: ({src, verse}) => null,
+
+  roSchmo: ({src, verse}) => null,
+
+
+  princess: ({src, verse}) => null,
+
+  book: ({src, verse}) => null,
+
+
+  music: ({src, verse}) => null,
+
+  review: ({src, verse}) => null,
+
+  default: ({src, verse}) => <svg xmlns="http://www.w3.org/2000/svg" xmlnsXlink="http://www.w3.org/1999/xlink" viewBox="0 0 143.17 108.06">
+    <defs>
+      <style>{".cls-1{fill:#7fc6e2;}.cls-2{fill:#fff;}.cls-3{font-size:10.22px;fill:#231f20;stroke-width:0.21px;font-family:Helvetica-Light, Helvetica;font-weight:300;}.cls-3,.cls-4{stroke:#231f20;stroke-miterlimit:10;}.cls-4{stroke-width:0.2px;opacity:0.3;fill:url(#linear-gradient);}.cls-5{fill:#00aeef;}"}</style>
+      <linearGradient id="linear-gradient" x1="-22.55" y1="150.45" x2="23.34" y2="150.45" gradientTransform="translate(71.09 -117.05)" gradientUnits="userSpaceOnUse">
+        <stop offset="0" stop-opacity="0"/>
+        <stop offset="1"/>
+      </linearGradient>
+    </defs>
+    <rect class="cls-1" x="3.33" y="5.78" width="135.7" height="96.24" rx="6.89"/>
+    <image width="3726" height="1996" transform="translate(7.31 10.49) scale(0.03 0.03)" xlinkHref={src}/>
+    <rect class="cls-2" x="7.5" y="56.36" width="127.62" height="41.31" rx="7.79"/>
+    <text class="cls-3" transform="translate(11.42 82.25) scale(0.99 1)">
+      {`${verse.book} ${verse.chapter}:${verse.verses}`}
+    </text>
+    <rect class="cls-4" x="48.54" y="-30.59" width="45.9" height="127.99" rx="6.73" transform="translate(38.08 104.9) rotate(-90)"/>
+    <ellipse class="cls-2" cx="70.95" cy="43.88" rx="13.95" ry="14.02"/>
+    <path class="cls-5" d="M69.45,48.75c-.69.4-1.25.08-1.26-.71l0-2.72,0-2.88,0-2.71c0-.79.55-1.13,1.24-.74l2.37,1.33,2.51,1.41,2.37,1.33a.76.76,0,0,1,0,1.44l-2.34,1.39-2.48,1.47Z"/>
+  </svg>,
+}
