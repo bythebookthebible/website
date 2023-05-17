678d402e08cfc92348bcd9f3524a309e9e3183a4
blackeaglebot <blackeaglebot@live.com>
2023-05-17 09:02:29 -0700

    base video element functionality - still needs settings controls

--- simpler/pages/index.js
+++ simpler/pages/index.js
@@ -1,8 +1,8 @@
 import './index.scss'
 import { loadFirebase } from '../sharedUI/firebase';
 import { getDownloadURL, getStorage, ref } from "firebase/storage";
-import { scriptureFromKey, keyFromScripture } from '../sharedUtil/dataUtil'
-import { defineAllDefaultTemplates, syncAttributeProperty, attachTemplateToShadow } from '../sharedUtil/uiUtil'
+import { scriptureFromKey, keyFromScripture, clamp } from '../sharedUtil/dataUtil'
+import { defineAllDefaultTemplates, syncAttributeProperty, attachTemplateToShadow, toggleFullscreen, isFullScreen, fullScreenEnabled } from '../sharedUtil/uiUtil'
 
 const cloudStorage = getStorage()
 import '../sharedUI/sharedWidgets'
@@ -234,6 +234,164 @@ window.customElements.define('video-page', class extends HTMLElement {
   }
 
   attributeChangedCallback(name, oldValue, newValue) {
+  }
+})
+
+
+/**
+ * Natural controlls (Play, pause, seeek, progress bar, download for offline, download base file, download derrivatives?, )
+ * Special controlls (Simple loops, Dragon loops, auto, chapter-long loops)
+ * Actual looping etc happens in web worker through MediaSource API https://developer.mozilla.org/en-US/docs/Web/API/MediaSource
+ * 
+ */
+
+window.customElements.define('custom-video', class extends HTMLElement {
+  static observedAttributes = [ ]
+
+  connectedCallback() {
+    syncAttributeProperty(this, 'src')  
+    attachTemplateToShadow(this, 'custom-video')
+
+    const el = this.elements = {
+      root: this.shadowRoot.querySelector('#root'),
+      overlay: this.shadowRoot.querySelector('#overlay'),
+      video: this.shadowRoot.querySelector('video'),
+
+      progressRoot: this.shadowRoot.querySelector('#progressRoot'),
+      progressTimestamp: this.shadowRoot.querySelector('#progressRoot .timestamp'),
+      progressBackground: this.shadowRoot.querySelector('#progressRoot .background'),
+      progressKnob: this.shadowRoot.querySelector('#progressRoot .knob'),
+      progressPreloaded: this.shadowRoot.querySelector('#progressRoot .preloaded'),
+      progress: this.shadowRoot.querySelector('#progressRoot .progress'),
+
+      play: this.shadowRoot.querySelector('#play'),
+
+      settings: this.shadowRoot.querySelector('#settings'),
+      download: this.shadowRoot.querySelector('#download'),
+      speed: this.shadowRoot.querySelector('#speed'),
+      repeatAuto: this.shadowRoot.querySelector('#repeat-auto'),
+      repeatLoop: this.shadowRoot.querySelector('#repeat-loop'),
+      repeatContinue: this.shadowRoot.querySelector('#repeat-continue'),
+
+      fullscreen: this.shadowRoot.querySelector('#fullscreen'),
+    }
+
+    // event handlers
+
+    // hide / show overlay
+    let overlayTimeout = undefined
+    
+    el.overlay.onmousemove = e => {
+      if(el.overlay.style.opacity == '0') {
+        // if opacity hidden, make visible
+        clearTimeout(overlayTimeout)
+        el.overlay.style.opacity = '1'
+      } else if (!el.video.paused) {
+        // if opacity visible & playing, set timeout to hide after 1 sec
+        console.log({overlayTimeout})
+        clearTimeout(overlayTimeout)
+        overlayTimeout = setTimeout(() => {
+          el.overlay.style.opacity = '0'
+        }, 700)
+      }
+    }
+
+    // play / pause
+    el.play.onclick = e => {
+      // unless on a specific control, toggle play
+      clearTimeout(overlayTimeout)
+      if(el.video.paused) {
+        el.video.play()
+        el.play.classList.replace('fa-play', 'fa-pause')
+        overlayTimeout = setTimeout(() => {
+          el.overlay.style.opacity = '0'
+        }, 300)
+      } else { 
+        el.video.pause()
+        el.play.classList.replace('fa-pause', 'fa-play')
+        el.overlay.style.opacity = '1'
+      }
+    }
+
+    // fullscreen
+    if(fullScreenEnabled()) {
+      el.fullscreen.onclick =  () => {
+        const fullscreen = toggleFullscreen(this)
+        el.fullscreen.classList.remove(`fa-${!fullscreen ? "minimize" : "maximize"}`)
+        el.fullscreen.classList.add(`fa-${fullscreen ? "minimize" : "maximize"}`)
+      }
+
+    } else {
+      el.fullscreen.style.display = "none"
+    }
+
+    // seek
+    function handleMouseSeek(e) {
+      // if valid, seek in the video
+      if(el.video.duration) {
+        let boundingBox = el.progressBackground.getBoundingClientRect()
+        let fraction = clamp((e.x - boundingBox.x) / boundingBox.width, 0, 1)
+        let time = el.video.duration * fraction
+
+        el.progress.style.width = `${fraction * 100}%`
+        el.video.currentTime = time
+      }
+    }
+
+    el.progressRoot.onmouseup = handleMouseSeek
+    el.progressRoot.onmousedown = handleMouseSeek
+    el.progressRoot.ontouchend = handleMouseSeek
+    el.progressRoot.ontouchmove = handleMouseSeek
+    el.progressRoot.onmousemove = e => {
+      if(e.buttons === 1) handleMouseSeek(e)
+    }
+
+    el.video.ontimeupdate = e => console.log(e)
+
+    // settings
+
+    // loop
+
+    // speed
+
+    // download
+
+    // get things for the video
+    let { memoryModules, memorySeries, memoryResources } = firebaseState.current
+    
+    // reorder memorySeries
+    const query = Object.fromEntries(new URL(window.location).searchParams) // {module: "", series: ""}
+
+    let { Schmideo, Music, ...other } = memorySeries
+    memorySeries = { Schmideo, Music, ...other }
+    
+    let relevantResources = Object.values(memoryResources).filter(val => val.module == query.module)
+    
+    // sort relevantResources in order of memorySeries
+    relevantResources = Object.keys(memorySeries)
+      .map(series => relevantResources.find(x => x.series == series))
+      .filter(x => x != undefined)
+
+
+    console.log({relevantResources, memoryModules, memorySeries})
+
+    let resource = relevantResources[0]
+
+    console.log({resource})
+
+    getDownloadURL(ref(cloudStorage, resource.location))
+      .then(url => {
+        console.log({url})
+        el.video.src = url
+      })
+      .catch(console.error)
+  }
+
+  attributeChangedCallback(name, oldValue, newValue) {
+    if(name === 'src' && oldValue != newValue) {
+      console.log(this.elements)
+      this.elements.video.src = newValue
+    }
   }
 })
 
